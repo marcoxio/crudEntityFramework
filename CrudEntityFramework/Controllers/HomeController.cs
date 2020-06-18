81f559c8b3ddc751512e2e59b24afef5a7a389d2
Marco Jimenez <manjiso2@hotmail.com>
2020-06-18 14:46:07 -0500

    Eliminar Vista Usuario

--- CrudEntityFramework/Controllers/HomeController.cs
+++ CrudEntityFramework/Controllers/HomeController.cs
@@ -94,6 +94,42 @@ namespace CrudEntityFramework.Controllers
             return View(usuario);
         }
 
+         [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = _context.Usuario.Find(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteRegistro(int? id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if(usuario == null)
+            {
+                return View();
+            }
+           
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));           
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
