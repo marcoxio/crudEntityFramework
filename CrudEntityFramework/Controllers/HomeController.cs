316def7edc7625ba08370f460417be3fd3f9019f
Marco Jimenez <manjiso2@hotmail.com>
2020-06-18 08:57:33 -0500

    Convirtiendo método Index en asincrono

--- CrudEntityFramework/Controllers/HomeController.cs
+++ CrudEntityFramework/Controllers/HomeController.cs
@@ -6,21 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CrudEntityFramework.Models;
+using CrudEntityFramework.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudEntityFramework.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext  context)
+        {
+            _context = context;
+        }
         private readonly ILogger<HomeController> _logger;
+        
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Usuario.ToListAsync());
         }
 
         public IActionResult Privacy()
